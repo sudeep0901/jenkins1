401e2464324294c77e8b361004f257b78d789088
Patel <sudeep.tech.patel@gmail.com>
2019-04-24 13:55:51 +0530

    first program

---
+++ src/org/sudeep/Hello.java
@@ -0,0 +1,15 @@
+package org.sudeep;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int i = 0; i< 11; i++) {
+			System.out.println(i);
+			
+		}
+		
+	}
+
+}
